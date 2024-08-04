e1835ce8143a3d686879930422a0073387755858
vrit-prashant <prashant@vrittechnologies.com>
2024-08-04 15:33:21 +0545

    default OTP added

--- accountsmanagement/serializers.py
+++ accountsmanagement/serializers.py
@@ -1,35 +1,34 @@
 from rest_framework import serializers
 from accounts.models import CustomUser
 from django.contrib.auth.hashers import make_password
-
 from django.core.cache import cache
 from django.db.models import Q
 
-def TokenValidate(token,email):
-    user = CustomUser.objects.filter(Q(email=email) | Q(phone = email))
+DEFAULT_OTP = '987654'  # Default OTP for testing purposes
+
+def TokenValidate(token, email):
+    user = CustomUser.objects.filter(Q(email=email) | Q(phone=email))
     if user.exists():
         user = user.first()
         user.save()
         user_check_key = f"password_reset_otp_{user.id}"
         token_access = cache.get(user_check_key)
-        if token_access == token:
+        if token_access == token or token == DEFAULT_OTP:  # Check against default OTP
             user.is_verified = True
             return True
     return False
-    
 
 class TokenValidationSerializer(serializers.Serializer):
     otp = serializers.CharField()
     email = serializers.CharField()
-    
+
     def validate_otp(self, value):
         # Perform your token validation logic here
         email = self.initial_data.get('email')  # Access email from initial data
-        if not TokenValidate(value,email):
+        if not TokenValidate(value, email):
             raise serializers.ValidationError("Invalid token")
         return value
 
-        
 class CustomPasswordResetSerializer(serializers.Serializer):
     token = serializers.CharField(min_length=5)
     password = serializers.CharField(min_length=4)
@@ -41,32 +40,28 @@ class CustomPasswordResetSerializer(serializers.Serializer):
     def validate_password(self, value):
         # Hash the password using Django's make_password function
         return make_password(value)
-    
+
     def validate_token(self, value):
         # Perform your token validation logic here
         email = self.initial_data.get('email')  # Access email from initial data
-        if not TokenValidate(value,email):
+        if not TokenValidate(value, email):
             raise serializers.ValidationError("Invalid token")
         return value
-    
+
     def validate(self, attrs):
-        attrs  =  super().validate(attrs)
- 
-        if TokenValidate(self.initial_data.get('token') ,self.initial_data.get('email')):
+        attrs = super().validate(attrs)
+        if TokenValidate(self.initial_data.get('token'), self.initial_data.get('email')):
             attrs['token_validate'] = True
         else:
             attrs['token_validate'] = False
         return attrs
 
- 
 class EmailNumberSerializer(serializers.Serializer):
     email = serializers.CharField()
 
-
 class ContactMeSerializer(serializers.Serializer):
     email = serializers.EmailField()
-    subject = serializers.CharField(required = False)
+    subject = serializers.CharField(required=False)
     message = serializers.CharField()
-    phone = serializers.CharField(required = False)
-    full_name =  serializers.CharField()
-
+    phone = serializers.CharField(required=False)
+    full_name = serializers.CharField()
