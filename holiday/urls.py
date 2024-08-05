055be16a6b4ec98c3ee2bd2f44ec4999795ed254
vrit-prashant <prashant@vrittechnologies.com>
2024-08-05 11:12:27 +0545

    router debugged

--- holiday/urls.py
+++ holiday/urls.py
@@ -6,7 +6,7 @@ router = DefaultRouter()
 
 router.register('holiday-trip', holidaytrip_viewsets.HolidayTripViewsets, basename="HolidayTripViewsets")
 router.register('holiday-trip-type', holidaytype_viewsets.HolidayTypeViewsets, basename="HolidayTypeViewsets")
-router.register('holiday-trip-type', holidaytripreview_viewsets.HolidayTripReviewViewsets, basename="HolidayTripReviewViewsets")
+router.register('holiday-trip-type-review', holidaytripreview_viewsets.HolidayTripReviewViewsets, basename="HolidayTripReviewViewsets")
 
 urlpatterns = [    
     # path('', include(router.urls)),
