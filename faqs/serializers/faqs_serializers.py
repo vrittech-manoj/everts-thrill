84447215aace5c014a25fcd503662896f02b04b1
Prashant Karna <prashant@vrittechnologies.com>
2024-08-16 15:04:06 +0545

    faq destination id added

--- faqs/serializers/faqs_serializers.py
+++ faqs/serializers/faqs_serializers.py
@@ -1,17 +1,30 @@
 from rest_framework import serializers
 from ..models import Faqs
+from destination.models import Destination
+
+class DestinationSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Destination
+        fields = '__all__'
 
 class FaqsListSerializers(serializers.ModelSerializer):
+    faqs_for_destination = DestinationSerializers(read_only=True)
     class Meta:
         model = Faqs
         fields = '__all__'
 
 class FaqsRetrieveSerializers(serializers.ModelSerializer):
+    faqs_for_destination = DestinationSerializers(read_only=True)
     class Meta:
         model = Faqs
         fields = '__all__'
 
 class FaqsWriteSerializers(serializers.ModelSerializer):
+    destinations_faq_detail = serializers.SerializerMethodField()
+
     class Meta:
         model = Faqs
         fields = '__all__'
+
+    def get_destinations_faq_detail(self, obj):
+        return DestinationSerializers(obj.destination).data
