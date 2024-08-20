d75bf930c7a2624f17f80189f39cf465566f7352
Prashant Karna <prashant@vrittechnologies.com>
2024-08-20 09:47:17 +0545

    legal documents serializers

--- company/serializers/legaldocuments_serializers.py
+++ company/serializers/legaldocuments_serializers.py
@@ -1,17 +1,28 @@
 from rest_framework import serializers
 from ..models import LegalDocuments
+from accounts.models import CustomUser
 
+class CustomUserReadSerializer(serializers.ModelSerializer):
+    class Meta:
+        ref_name =  "account_legaldocuments"
+        model = CustomUser
+        # fields = '__all__' 
+        exclude = ['password']
+        
 class LegalDocumentsListSerializers(serializers.ModelSerializer):
+    user_legal_documents = CustomUserReadSerializer(read_only = True)
     class Meta:
         model = LegalDocuments
         fields = '__all__'
 
 class LegalDocumentsRetrieveSerializers(serializers.ModelSerializer):
+    user_legal_documents = CustomUserReadSerializer(read_only = True)
     class Meta:
         model = LegalDocuments
         fields = '__all__'
 
 class LegalDocumentsWriteSerializers(serializers.ModelSerializer):
+    # user_legal_documents = CustomUserReadSerializer()
     class Meta:
         model = LegalDocuments
         fields = '__all__'
