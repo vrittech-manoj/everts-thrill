1c9ce6ce9c84e14041b3bb76d7d08b71a0e15994
Prashant Karna <prashant@vrittechnologies.com>
2024-08-28 10:26:56 +0545

    updated destination

--- destination/viewsets/destination_viewsets.py
+++ destination/viewsets/destination_viewsets.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, permissions, filters
 from ..models import Destination
+
 from django.shortcuts import get_object_or_404
 from ..serializers.destination_serializers import (
     DestinationlistUserSerializers,
@@ -7,6 +8,8 @@ from ..serializers.destination_serializers import (
     DestinationRetrieveUserSerializers,
     DestinationRetrieveAdminSerializers,
     DestinationWriteSerializers
+    
+
 )
 from ..utilities.importbase import *
 from ..utilities.permissions import destinationPermission
@@ -39,7 +42,7 @@ class DestinationViewsets(viewsets.ModelViewSet):
 
     filter_backends = [SearchFilter, DjangoFilterBackend, OrderingFilter]
     search_fields = ['destination_title']
-    ordering_fields = ['destination_title', 'id', 'duration']
+    ordering_fields = ['destination_title', 'id','duration']
     filterset_class = DestinationFilter  # Use the custom filter set here
     lookup_field = "slug"
 
@@ -47,12 +50,12 @@ class DestinationViewsets(viewsets.ModelViewSet):
         if self.action in ['create', 'update', 'partial_update']:
             return DestinationWriteSerializers
         elif self.action == "list":
-            if self.request.user.is_authenticated and her.request.user.role in [roles.ADMIN, roles.SUPER_ADMIN]:
+            if self.request.user.is_authenticated and self.request.user.role in [roles.ADMIN, roles.SUPER_ADMIN]:
                 return DestinationlistAdminSerializers
             else:
                 return DestinationlistUserSerializers
         elif self.action == "retrieve":
-            if self.request.user.is_authenticated and her.request.user.role in [roles.ADMIN, roles.SUPER_ADMIN]:
+            if self.request.user.is_authenticated and self.request.user.role in [roles.ADMIN, roles.SUPER_ADMIN]:
                 return DestinationRetrieveAdminSerializers
             else:
                 return DestinationRetrieveUserSerializers
