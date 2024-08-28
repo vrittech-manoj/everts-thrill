d9cfbb903f412f3012cd654c9dae367cf912df66
Prashant Karna <prashant@vrittechnologies.com>
2024-08-28 13:07:57 +0545

    "Updated destination_filter.py and export.py files with changes to filter methods and CSV export functionality."

--- destination/utilities/destination_filter.py
+++ destination/utilities/destination_filter.py
@@ -1,48 +1,33 @@
 import django_filters
 from ..models import Destination
-import ast
 
 class DestinationFilter(django_filters.FilterSet):
     activities = django_filters.CharFilter(method='filter_by_activities')
     collections = django_filters.CharFilter(method='filter_by_collections')
     packages = django_filters.CharFilter(method='filter_by_packages')
-    
 
     class Meta:
         model = Destination
         fields = {
             'destination_title': ['exact', 'icontains'],
             'nature_of_trip': ['exact', 'icontains'],
-            'duration': ['exact','gte','lte'],
-            
+            'duration': ['exact', 'gte', 'lte'],
         }
 
     def filter_by_collections(self, queryset, name, value):
-        try:
-            collections = self.request.GET.get('collections')
-            collections = collections.split(',')
-            queryset = queryset.filter(collections_id__in=collections)
-        except:
-            pass
-        return queryset
-    
-    
-    def filter_by_activities(self, queryset, name, value):
-        try:
-            activities = self.request.GET.get('activities')
-            activities = activities.split(',')
-            queryset = queryset.filter(activities_id__in=activities)
-        except:
-            pass
-        return queryset
-    
-    
-    def filter_by_packages(self, queryset, name, value):
-        try:
-            packages = self.request.GET.get('packages')
-            packages = packages.split(',')
-            queryset = queryset.filter(packages_id__in=packages)
-        except:
-            pass
+        if value:
+            collections = value.split(',') if ',' in value else [value]
+            queryset = queryset.filter(collections__id__in=collections)
         return queryset
 
+    def filter_by_activities(self, queryset, name, value):
+        if value:
+            activities = value.split(',') if ',' in value else [value]
+            queryset = queryset.filter(activities__id__in=activities)
+        return queryset
+
+    def filter_by_packages(self, queryset, name, value):
+        if value:
+            packages = value.split(',') if ',' in value else [value]
+            queryset = queryset.filter(packages__id__in=packages)
+        return queryset
