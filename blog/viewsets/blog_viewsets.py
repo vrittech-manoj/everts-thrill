12b3705e102fc1c67814e2474227792adec01b89
Prashant Karna <prashant@vrittechnologies.com>
2024-09-01 12:07:24 +0545

    blog

--- blog/viewsets/blog_viewsets.py
+++ blog/viewsets/blog_viewsets.py
@@ -4,29 +4,27 @@ from ..utilities.importbase import *
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticated
 
 class BlogViewSets(viewsets.ModelViewSet):
-    serializer_class = BlogListSerializers
-    permission_classes = [AdminViewSetsPermission]
+    permission_classes = [IsAuthenticated] 
     pagination_class = MyPageNumberPagination
-    queryset = Blog.objects.all().order_by("created_date")
-    
     filter_backends = [SearchFilter, DjangoFilterBackend, OrderingFilter]
     search_fields = ['title']
-    ordering_fields = ['id', 'title','created_date']
+    ordering_fields = ['id', 'title', 'created_date']
+    queryset = Blog.objects.all().order_by("created_date") 
 
     filterset_fields = {
         'title': ['exact'],
         'is_popular': ['exact'],
     }
-    
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        return queryset
 
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update']:
             return BlogWriteSerializers
         elif self.action == 'retrieve':
             return BlogRetrieveSerializers
-        return super().get_serializer_class()
+        return BlogListSerializers  #
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
