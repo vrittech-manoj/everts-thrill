b99ff38ff92d86ade1ff822851b2f462ebf976cf
Prashant Karna <prashant@vrittechnologies.com>
2024-09-01 12:16:41 +0545

    popular

--- blog/viewsets/blog_viewsets.py
+++ blog/viewsets/blog_viewsets.py
@@ -12,19 +12,30 @@ class BlogViewSets(viewsets.ModelViewSet):
     filter_backends = [SearchFilter, DjangoFilterBackend, OrderingFilter]
     search_fields = ['title']
     ordering_fields = ['id', 'title', 'created_date']
-    queryset = Blog.objects.all().order_by("created_date") 
-
+    
     filterset_fields = {
         'title': ['exact'],
         'is_popular': ['exact'],
     }
+
+    def get_queryset(self):
+        queryset = Blog.objects.all().order_by("created_date")
+        
+        # Check if 'is_popular' filter is present in the query parameters
+        is_popular_filter = self.request.query_params.get('is_popular')
+        
+        if is_popular_filter is not None:
+            # Filter the queryset to only include popular blogs
+            queryset = queryset.filter(is_popular=True)
+        
+        return queryset
 
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update']:
             return BlogWriteSerializers
         elif self.action == 'retrieve':
             return BlogRetrieveSerializers
-        return BlogListSerializers  #
+        return BlogListSerializers  # Default serializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
