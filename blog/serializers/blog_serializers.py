4b7577f4437aec00b8297cf7426a5a9f0b642f4e
Prashant Karna <prashant@vrittechnologies.com>
2024-09-01 12:04:51 +0545

    blog

--- blog/serializers/blog_serializers.py
+++ blog/serializers/blog_serializers.py
@@ -13,17 +13,24 @@ class BlogListSerializers(serializers.ModelSerializer):
     class Meta:
         model = Blog
         fields = '__all__'
-        
+
 class BlogRetrieveSerializers(serializers.ModelSerializer):
     user = BlogUserSerializer(read_only=True)
 
     class Meta:
         model = Blog
         fields = '__all__'
-       
 
 class BlogWriteSerializers(serializers.ModelSerializer):
     class Meta:
         model = Blog
         fields = '__all__'
+        read_only_fields = ['user']  
 
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user  
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data['user'] = self.context['request'].user  
+        return super().update(instance, validated_data)
