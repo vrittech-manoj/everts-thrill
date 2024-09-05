bd7d9ce61e9314f0d5afcd88ecc95b06db5e4d38
Prashant Karna <prashant@vrittechnologies.com>
2024-09-05 17:01:50 +0545

    revert

--- faqs/serializers/faqs_serializers.py
+++ faqs/serializers/faqs_serializers.py
@@ -5,7 +5,7 @@ from destination.models import Destination
 class DestinationSerializers(serializers.ModelSerializer):
     class Meta:
         model = Destination
-        fields = ['destination_title','id']
+        fields = '__all__'
         ref_name = 'FaqsDestination'
 
 class FaqsListSerializers(serializers.ModelSerializer):
@@ -21,6 +21,11 @@ class FaqsRetrieveSerializers(serializers.ModelSerializer):
         fields = '__all__'
 
 class FaqsWriteSerializers(serializers.ModelSerializer):
+    destinations_faq_detail = serializers.SerializerMethodField()
+
     class Meta:
         model = Faqs
         fields = '__all__'
+
+    def get_destinations_faq_detail(self, obj):
+        return DestinationSerializers(obj.destination).data
