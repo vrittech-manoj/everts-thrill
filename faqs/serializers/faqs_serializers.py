4ce56ea61dc2795c7f3a1907887e2b665044eabb
Prashant Karna <prashant@vrittechnologies.com>
2024-09-06 07:21:31 +0545

    faw

--- faqs/serializers/faqs_serializers.py
+++ faqs/serializers/faqs_serializers.py
@@ -8,24 +8,46 @@ class DestinationSerializers(serializers.ModelSerializer):
         fields = '__all__'
         ref_name = 'FaqsDestination'
 
+
 class FaqsListSerializers(serializers.ModelSerializer):
     faqs_for_destination = DestinationSerializers(read_only=True)
+
     class Meta:
         model = Faqs
         fields = '__all__'
+
 
 class FaqsRetrieveSerializers(serializers.ModelSerializer):
     faqs_for_destination = DestinationSerializers(read_only=True)
+
     class Meta:
         model = Faqs
         fields = '__all__'
+
 
 class FaqsWriteSerializers(serializers.ModelSerializer):
-    destinations_faq_detail = serializers.SerializerMethodField()
+    faqs_for_destination = DestinationSerializers()
 
     class Meta:
         model = Faqs
         fields = '__all__'
 
-    def get_destinations_faq_detail(self, obj):
-        return DestinationSerializers(obj.destination).data
+    # def create(self, validated_data):
+    #     destination_data = validated_data.pop('faqs_for_destination')
+    #     destination_instance = Destination.objects.create(**destination_data)
+    #     faqs_instance = Faqs.objects.create(faqs_for_destination=destination_instance, **validated_data)
+    #     return faqs_instance
+
+    # def update(self, instance, validated_data):
+    #     destination_data = validated_data.pop('faqs_for_destination', None)
+
+    #     if destination_data:
+    #         destination_instance = instance.faqs_for_destination
+    #         for key, value in destination_data.items():
+    #             setattr(destination_instance, key, value)
+    #         destination_instance.save()
+
+    #     for attr, value in validated_data.items():
+    #         setattr(instance, attr, value)
+    #     instance.save()
+    #     return instance
