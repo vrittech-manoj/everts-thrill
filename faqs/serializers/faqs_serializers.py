c4ce1bf5fd0e46d67d9e53b7cddc3cc7fdd3efc1
Prashant Karna <prashant@vrittechnologies.com>
2024-09-06 07:31:05 +0545

    serializers

--- faqs/serializers/faqs_serializers.py
+++ faqs/serializers/faqs_serializers.py
@@ -5,49 +5,30 @@ from destination.models import Destination
 class DestinationSerializers(serializers.ModelSerializer):
     class Meta:
         model = Destination
-        fields = '__all__'
+        fields = ['destination_title','id']
         ref_name = 'FaqsDestination'
 
 
 class FaqsListSerializers(serializers.ModelSerializer):
-    faqs_for_destination = DestinationSerializers(many = True,read_only=True)
+    destination = DestinationSerializers(many = True,read_only=True)
 
     class Meta:
         model = Faqs
-        fields = '__all__'
+        fields = ('title','id' 'description', 'faq_type', 'destination', 'created_at', )
 
 
 class FaqsRetrieveSerializers(serializers.ModelSerializer):
-    faqs_for_destination = DestinationSerializers(many = True,read_only=True)
-
+    destination = DestinationSerializers(many = True,read_only=True)
     class Meta:
         model = Faqs
-        fields = '__all__'
+        fields = ('title','id' 'description', 'faq_type', 'destination', 'created_at', )
 
 
 class FaqsWriteSerializers(serializers.ModelSerializer):
-    faqs_for_destination = DestinationSerializers(many = True)
+    # faqs_for_destination = DestinationSerializers(many = True)
 
     class Meta:
         model = Faqs
         fields = '__all__'
 
-    # def create(self, validated_data):
-    #     destination_data = validated_data.pop('faqs_for_destination')
-    #     destination_instance = Destination.objects.create(**destination_data)
-    #     faqs_instance = Faqs.objects.create(faqs_for_destination=destination_instance, **validated_data)
-    #     return faqs_instance
-
-    # def update(self, instance, validated_data):
-    #     destination_data = validated_data.pop('faqs_for_destination', None)
-
-    #     if destination_data:
-    #         destination_instance = instance.faqs_for_destination
-    #         for key, value in destination_data.items():
-    #             setattr(destination_instance, key, value)
-    #         destination_instance.save()
-
-    #     for attr, value in validated_data.items():
-    #         setattr(instance, attr, value)
-    #     instance.save()
-    #     return instance
+    
