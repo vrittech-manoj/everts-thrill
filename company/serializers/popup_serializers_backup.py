ee9d49c1bdb686a939809bc485f4fdcf53f29c63
Prashant Karna <prashant@vrittechnologies.com>
2024-09-08 10:40:24 +0545

    popup

--- company/serializers/popup_serializers_backup.py
+++ company/serializers/popup_serializers_backup.py
@@ -20,26 +20,46 @@ class PopupWriteSerializers(serializers.ModelSerializer):
         popups = []
         request = self.context['request']
 
-        index = 0
-        while f'data[{index}][title]' in request.data:
-            title = request.data.get(f'data[{index}][title]')
-            url = request.data.get(f'data[{index}][url]')
-            image = request.FILES.get(f'data[{index}][image]', None)
+        # Check if the request data is a list (array) or a single object
+        if isinstance(request.data.get('data'), list):
+            # Handle array type data
+            index = 0
+            while f'data[{index}][title]' in request.data:
+                title = request.data.get(f'data[{index}][title]')
+                url = request.data.get(f'data[{index}][url]')
+                image = request.FILES.get(f'data[{index}][image]', None)
+
+                if not title:
+                    raise serializers.ValidationError(f"Title is required for popup {index}.")
+
+                popup_instance = self.create_popup_instance(title, url, image)
+                popups.append(popup_instance)
+                index += 1
+        else:
+            # Handle single object data
+            title = request.data.get('data[title]')
+            url = request.data.get('data[url]')
+            image = request.FILES.get('data[image]', None)
 
             if not title:
-                raise serializers.ValidationError(f"Title is required for popup {index}.")
+                raise serializers.ValidationError("Title is required for popup.")
 
-            if image:
-                popup_instance = Popup.objects.create(title=title, image=image, url=url)
-                popup_instance.url = popup_instance.image.url
-            else:
-                popup_instance = Popup.objects.create(title=title, url=url)
-
-            popup_instance.save()
+            popup_instance = self.create_popup_instance(title, url, image)
             popups.append(popup_instance)
-            index += 1
-# TODO return all the data in payload
+
+        # Return all the data in the payload
         return popups[0]
+
+    def create_popup_instance(self, title, url, image):
+        """Helper function to create a Popup instance."""
+        if image:
+            popup_instance = Popup.objects.create(title=title, image=image, url=url)
+            popup_instance.url = popup_instance.image.url
+        else:
+            popup_instance = Popup.objects.create(title=title, url=url)
+
+        popup_instance.save()
+        return popup_instance
 
     def to_representation(self, instance):
         """Convert to a format that matches the original request structure."""
