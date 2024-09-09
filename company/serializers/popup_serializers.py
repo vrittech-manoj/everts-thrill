0b013467d84e82362b0dee0d21c488dd006a777f
Prashant Karna <prashant@vrittechnologies.com>
2024-09-09 12:05:08 +0545

    popupp

--- company/serializers/popup_serializers.py
+++ company/serializers/popup_serializers.py
@@ -38,21 +38,19 @@ class PopupWriteSerializers(serializers.ModelSerializer):
                     popup_instance = Popup.objects.get(id=popup_id)
                     popup_instance.title = title
                     popup_instance.url = url
-                    popup_instance.image = image
-                    # if image:
-                    #     popup_instance.image = image
+
+                    # Only update the image if a new image is provided
+                    if image:
+                        popup_instance.image = image
+
                     popup_instance.save()
                 except Popup.DoesNotExist:
                     print(f"Popup with id {popup_id} does not exist.")
                     index += 1
                     continue
             else:
+                # Create a new Popup instance with or without an image
                 popup_instance = Popup.objects.create(title=title, image=image, url=url)
-                # # Create a new Popup instance
-                # if image:
-                #     popup_instance = Popup.objects.create(title=title, image=image, url=url)
-                # else:
-                #     popup_instance = Popup.objects.create(title=title,image=image, url=url)
 
             popups.append(popup_instance)
             index += 1
@@ -61,6 +59,7 @@ class PopupWriteSerializers(serializers.ModelSerializer):
             return popups[0] if popups else []
         except:
             return []
+
 
     def to_representation(self, instance):
         """Convert to a format that matches the original request structure."""
