3c4a526d48dcb7fdf7120f602a09cc012241ad1b
Prashant Karna <prashant@vrittechnologies.com>
2024-09-09 11:36:35 +0545

    max limit

--- company/viewsets/popup_viewsets.py
+++ company/viewsets/popup_viewsets.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from ..models import Popup
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from ..serializers.popup_serializers import PopupListSerializers, PopupRetrieveSerializers, PopupWriteSerializers
 
 class popupViewsets(viewsets.ModelViewSet):
@@ -16,4 +18,15 @@ class popupViewsets(viewsets.ModelViewSet):
 
 
     def perform_create(self, serializer):
-        return serializer.save()
+        # Check title length before saving
+        title = serializer.validated_data.get('title', '')
+        if len(title) > 200:
+            return Response({'error': 'Title exceeds the maximum length of 200 characters.'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+
+    def perform_update(self, serializer):
+        # Check title length before updating
+        title = serializer.validated_data.get('title', '')
+        if len(title) > 200:
+            return Response({'error': 'Title exceeds the maximum length of 200 characters.'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
