147caceb1cfceb74ccb22bfbc9d5fb6cb7153530
Prashant Karna <prashant@vrittechnologies.com>
2024-09-11 15:21:52 +0545

    cicd

--- trek/settings.py
+++ trek/settings.py
@@ -96,9 +96,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -248,11 +245,3 @@ INTERNAL_IPS = [
 
 SMS_KEY_PASSWORD = ''
 
-if not DEBUG:
-    SECURE_SSL_REDIRECT = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
