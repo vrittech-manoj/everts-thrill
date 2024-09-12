883b00922fcb0d69072df9ba145fa4628e1e7089
Prashant Karna <prashant@vrittechnologies.com>
2024-09-12 12:04:43 +0545

    multiple data saved at once in stats

--- company/admin.py
+++ company/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Popup, MeetTeam, TermAndCondition, VisaInformation,AboutUs, LegalDocuments,HeroSection,PrivacyPolicy,HeroSectionOverlay
+from .models import Popup, MeetTeam,HeroSectionStats, TermAndCondition, VisaInformation,AboutUs, LegalDocuments,HeroSection,PrivacyPolicy,HeroSectionOverlay
 
 # Register your models here.
 admin.site.register(Popup)
@@ -11,3 +11,4 @@ admin.site.register(HeroSection)
 admin.site.register(PrivacyPolicy)
 admin.site.register(HeroSectionOverlay)
 admin.site.register(AboutUs)
+admin.site.register(HeroSectionStats)
