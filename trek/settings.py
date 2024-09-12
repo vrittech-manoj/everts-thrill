8d761b753b60a3671b087d6a8f7e2e891148f3a8
vrittech-manoj <manoj@vrittechnologies.com>
2024-09-12 12:41:40 +0545

    media

--- trek/settings.py
+++ trek/settings.py
@@ -186,7 +186,6 @@ MEDIA_URL = 'https://api.everestthrills.com/media/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-MEDIA_URL = 'media/'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
