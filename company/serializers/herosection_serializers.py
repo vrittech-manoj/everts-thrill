dd5774cddc3f9bff02d9e6cbe9820b2ee380196a
vrittech-manoj <manoj@vrittechnologies.com>
2024-09-12 13:21:02 +0545

    media

--- company/serializers/herosection_serializers.py
+++ company/serializers/herosection_serializers.py
@@ -11,6 +11,23 @@ class HeroSectionRetrieveSerializers(serializers.ModelSerializer):
         model = HeroSection
         fields = '__all__'
 
+class HeroSectionRetrieveSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = HeroSection
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        
+        # Check if the 'video' field exists and is a string
+        video_url = data.get('video', '')
+        if isinstance(video_url, str):
+            # Replace 'http' with 'https' only if it exists in the string
+            data['video'] = video_url.replace('http', 'https', 1)
+
+        return data
+
+
 class HeroSectionWriteSerializers(serializers.ModelSerializer):
     class Meta:
         model = HeroSection
