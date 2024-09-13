3e0bf14b8acd43e444acc907813ba1534cefe4c3
Prashant Karna <prashant@vrittechnologies.com>
2024-09-13 13:05:29 +0545

    multiple data of hero section stats

--- company/serializers/herosectionstats_serializers.py
+++ company/serializers/herosectionstats_serializers.py
@@ -3,6 +3,14 @@ from ..models import HeroSectionStats
 import ast
 from django.db import transaction
 
+from django.db import transaction
+
+from rest_framework import serializers
+from django.db import transaction
+from ..models import HeroSectionStats
+import ast
+
+
 class HeroSectionStatsListSerializers(serializers.ModelSerializer):
     class Meta:
         model = HeroSectionStats
@@ -18,55 +26,5 @@ class HeroSectionStatsWriteSerializers(serializers.ModelSerializer):
         model = HeroSectionStats
         fields = '__all__'
 
-    def to_internal_value(self, data):
-        """
-        Converts the incoming nested dictionary (or list of dictionaries) into 
-        a format suitable for the serializer.
-        """
-        if isinstance(data, dict):
-            stats = data.get('stats', [])
-            if isinstance(stats, list):
-                return [super().to_internal_value(item) for item in stats]
-        return super().to_internal_value(data)
 
-    @transaction.atomic
-    def create(self, validated_data):
-        """
-        Handles bulk creation if validated_data is a list of dictionaries.
-        """
-        if isinstance(validated_data, list):
-            stats = HeroSectionStats.objects.bulk_create(
-                [HeroSectionStats(**item) for item in validated_data]
-            )
-            return stats
-        else:
-            stat = HeroSectionStats.objects.create(**validated_data)
-            return stat
-
-    @transaction.atomic
-    def update(self, instance, validated_data):
-        """
-        Handles bulk update if validated_data is a list.
-        """
-        if isinstance(validated_data, list):
-            existing_stats = {stat.id: stat for stat in instance}
-            updated_stats = []
-
-            for stat_data in validated_data:
-                stat_id = stat_data.get('id', None)
-                if stat_id and stat_id in existing_stats:
-                    stat_instance = existing_stats[stat_id]
-                    for attr, value in stat_data.items():
-                        setattr(stat_instance, attr, value)
-                    stat_instance.save()
-                    updated_stats.append(stat_instance)
-                else:
-                    new_stat = HeroSectionStats.objects.create(**stat_data)
-                    updated_stats.append(new_stat)
-
-            return updated_stats
-        else:
-            for attr, value in validated_data.items():
-                setattr(instance, attr, value)
-            instance.save()
-            return instance
+    
--- company/viewsets/herosectionstats_viewsets.py
+++ company/viewsets/herosectionstats_viewsets.py
@@ -4,6 +4,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 from ..models import HeroSectionStats
 from ..serializers.herosectionstats_serializers import HeroSectionStatsListSerializers, HeroSectionStatsRetrieveSerializers, HeroSectionStatsWriteSerializers
 from ..utilities.importbase import *
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
+
 
 class herosectionstatsViewsets(viewsets.ModelViewSet):
     serializer_class = HeroSectionStatsListSerializers
@@ -30,8 +34,49 @@ class herosectionstatsViewsets(viewsets.ModelViewSet):
         elif self.action == 'retrieve':
             return HeroSectionStatsRetrieveSerializers
         return super().get_serializer_class()
+    def create(self, request, *args, **kwargs):
+        # Check if the request data is a list (indicating multiple objects)
+        data = request.data['stats']
+        if isinstance(data, list):
+            serializer = self.get_serializer(data=data, many=True)
+        else:
+            serializer = self.get_serializer(data=data)
 
-    # @action(detail=False, methods=['get'], name="action_name", url_path="url_path")
-    # def action_name(self, request, *args, **kwargs):
-    #     return super().list(request, *args, **kwargs)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+    def update(self, request, *args, **kwargs):
+        # Check if the request data is a list (indicating multiple objects)
+        data = request.data['stats']
+        
+        if isinstance(data, list):
+            # For bulk update, each item should have an ID
+            for item in data:
+                if 'id' not in item:
+                    return Response({"detail": "ID is required for each object in bulk update"}, status=status.HTTP_400_BAD_REQUEST)
+            
+            # Retrieve the queryset of the objects to update
+            partial = kwargs.pop('partial', False)
+            instance_ids = [item['id'] for item in data]
+            instances = self.get_queryset().filter(id__in=instance_ids)
 
+            if len(instances) != len(data):
+                return Response({"detail": "Some objects do not exist"}, status=status.HTTP_400_BAD_REQUEST)
+
+            serializer = self.get_serializer(instances, data=data, many=True, partial=partial)
+        else:
+            # Single update: ID must be provided
+            if 'id' not in data:
+                return Response({"detail": "ID is required for update"}, status=status.HTTP_400_BAD_REQUEST)
+            
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, data=data, partial=kwargs.get('partial', False))
+
+        # Validate and perform the update
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
