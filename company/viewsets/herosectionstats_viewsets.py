f06122ed7d3102268b94ececf4c5d9c12c000aad
Prashant Karna <prashant@vrittechnologies.com>
2024-09-13 17:54:27 +0545

    viewsets updated

--- company/viewsets/herosectionstats_viewsets.py
+++ company/viewsets/herosectionstats_viewsets.py
@@ -35,48 +35,37 @@ class herosectionstatsViewsets(viewsets.ModelViewSet):
             return HeroSectionStatsRetrieveSerializers
         return super().get_serializer_class()
     def create(self, request, *args, **kwargs):
-        # Check if the request data is a list (indicating multiple objects)
         data = request.data['stats']
+        
+        # Check if the request data is a list (indicating multiple objects)
         if isinstance(data, list):
-            serializer = self.get_serializer(data=data, many=True)
-        else:
-            serializer = self.get_serializer(data=data)
+            for item in data:
+                if 'id' in item:
+                    instance = self.get_queryset().filter(id=item['id']).first()
+                    if instance:
+                        serializer = self.get_serializer(instance, data=item, partial=True)
+                    else:
+                        serializer = self.get_serializer(data=item)
+                else:
+                    serializer = self.get_serializer(data=item)
 
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+                serializer.is_valid(raise_exception=True)
+                self.perform_create(serializer)
+                
+        else:
+            if 'id' in data:
+                instance = self.get_queryset().filter(id=data['id']).first()
+                if instance:
+                    serializer = self.get_serializer(instance, data=data, partial=True)
+                else:
+                    serializer = self.get_serializer(data=data)
+            else:
+                serializer = self.get_serializer(data=data)
+
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
         
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
     
-    def update(self, request, *args, **kwargs):
-        # Check if the request data is a list (indicating multiple objects)
-        data = request.data['stats']
-        
-        if isinstance(data, list):
-            # For bulk update, each item should have an ID
-            for item in data:
-                if 'id' not in item:
-                    return Response({"detail": "ID is required for each object in bulk update"}, status=status.HTTP_400_BAD_REQUEST)
-            
-            # Retrieve the queryset of the objects to update
-            partial = kwargs.pop('partial', False)
-            instance_ids = [item['id'] for item in data]
-            instances = self.get_queryset().filter(id__in=instance_ids)
-
-            if len(instances) != len(data):
-                return Response({"detail": "Some objects do not exist"}, status=status.HTTP_400_BAD_REQUEST)
-
-            serializer = self.get_serializer(instances, data=data, many=True, partial=partial)
-        else:
-            # Single update: ID must be provided
-            if 'id' not in data:
-                return Response({"detail": "ID is required for update"}, status=status.HTTP_400_BAD_REQUEST)
-            
-            instance = self.get_object()
-            serializer = self.get_serializer(instance, data=data, partial=kwargs.get('partial', False))
-
-        # Validate and perform the update
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
