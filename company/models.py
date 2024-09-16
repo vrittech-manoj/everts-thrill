c635a2c3f655e12436dfca0c484debe69aa04659
Prashant Karna <prashant@vrittechnologies.com>
2024-09-16 15:16:38 +0545

    model updated

--- company/models.py
+++ company/models.py
@@ -17,16 +17,33 @@ class Popup(models.Model):
             raise ValidationError({'title': 'The title cannot exceed 200 characters.'})
         super().save(*args, **kwargs)
 
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
 class MeetTeam(models.Model):
-    member_name = models.CharField(max_length = 200,null = True)
-    index = models.PositiveIntegerField(default = 999,unique=True)
-    position = models.CharField(max_length = 200,null = True)
+    member_name = models.CharField(max_length=200, null=True)
+    index = models.PositiveIntegerField(default=999)
+    position = models.CharField(max_length=200, null=True)
     description = models.TextField()
     image = models.ImageField(upload_to='components/meetteam', null=True, blank=True)
     url = models.URLField(null=True, blank=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f"{self.member_name}-{self.id}-{self.index}"
+
+    def clean(self):
+        # Check if another object with the same index exists
+        if MeetTeam.objects.filter(index=self.index).exclude(id=self.id).exists():
+            raise ValidationError(
+                _('An object with index %(index)s already exists. Please choose a different index.'),
+                params={'index': self.index},
+            )
+
+    def save(self, *args, **kwargs):
+        # Call clean method to check for index validation
+        self.clean()
+        super(MeetTeam, self).save(*args, **kwargs)
+
 
 class PrivacyPolicy(models.Model):
     description = models.TextField()
