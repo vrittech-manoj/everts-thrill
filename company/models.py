97150e25884938285b0ad2802d891197ccba0c8b
vrittech-manoj <manoj@vrittechnologies.com>
2024-09-22 21:07:32 +0545

    model re imported remove

--- company/models.py
+++ company/models.py
@@ -20,16 +20,6 @@ class Popup(models.Model):
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-class MeetTeam(models.Model):
-    member_name = models.CharField(max_length=200, null=True)
-    index = models.PositiveIntegerField(default=999)
-    position = models.CharField(max_length=200, null=True)
-    description = models.TextField()
-    image = models.ImageField(upload_to='components/meetteam', null=True, blank=True)
-    url = models.URLField(null=True, blank=True)
-
-    def __str__(self):
-        return f"{self.member_name}-{self.id}-{self.index}"
 
    
 
