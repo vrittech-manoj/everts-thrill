f2624cd4b9c6eb9bec5027384f0c7bfd8a50e2ab
Prashant Karna <prashant@vrittechnologies.com>
2024-09-22 15:04:44 +0545

    gallery images

--- destination/viewsets/destinationgalleryimages_viewsets.py
+++ destination/viewsets/destinationgalleryimages_viewsets.py
@@ -21,7 +21,7 @@ class destinationgalleryimagesViewsets(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        #return queryset.filter(user_id=self.request.user.id)
+        return queryset
 
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update']:
