5e808db26d6cb64e96f922b39bba79e659169f18
Prashant Karna <prashant@vrittechnologies.com>
2024-09-23 15:32:57 +0545

    hero section video

--- company/serializers/herosection_serializers.py
+++ company/serializers/herosection_serializers.py
@@ -36,6 +36,20 @@ class HeroSectionRetrieveSerializers(serializers.ModelSerializer):
     #         return data
     #     except:
     #         return data
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        try:
+            # Check if the 'video' field exists and is a string
+            video_url = data.get('video', '')
+            if isinstance(video_url, str) and video_url.startswith('http://'):
+                # Replace 'http' with 'https' only if it starts with 'http'
+                data['video'] = video_url.replace('http', 'https', 1)
+
+            return data
+        except Exception as e:
+            # Optionally log the exception e
+            return data
+
 
 class HeroSectionWriteSerializers(serializers.ModelSerializer):
     class Meta:
