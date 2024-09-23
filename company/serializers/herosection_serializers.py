ba313d019d282c553b1fc783bb740c585ed1ec37
Prashant Karna <prashant@vrittechnologies.com>
2024-09-23 15:40:00 +0545

    cicd

--- company/serializers/herosection_serializers.py
+++ company/serializers/herosection_serializers.py
@@ -24,31 +24,31 @@ class HeroSectionRetrieveSerializers(serializers.ModelSerializer):
         model = HeroSection
         fields = '__all__'
 
-    # def to_representation(self, instance):
-    #     data = super().to_representation(instance)
-    #     try:            
-    #         # Check if the 'video' field exists and is a string
-    #         video_url = data.get('video', '')
-    #         if isinstance(video_url, str):
-    #             # Replace 'http' with 'https' only if it exists in the string
-    #             data['video'] = video_url.replace('http', 'https', 1)
-
-    #         return data
-    #     except:
-    #         return data
     def to_representation(self, instance):
         data = super().to_representation(instance)
-        try:
+        try:            
             # Check if the 'video' field exists and is a string
             video_url = data.get('video', '')
-            if isinstance(video_url, str) and video_url.startswith('http://'):
-                # Replace 'http' with 'https' only if it starts with 'http'
+            if isinstance(video_url, str):
+                # Replace 'http' with 'https' only if it exists in the string
                 data['video'] = video_url.replace('http', 'https', 1)
 
             return data
-        except Exception as e:
-            # Optionally log the exception e
+        except:
             return data
+    # def to_representation(self, instance):
+    #     data = super().to_representation(instance)
+    #     try:
+    #         # Check if the 'video' field exists and is a string
+    #         video_url = data.get('video', '')
+    #         if isinstance(video_url, str) and video_url.startswith('http://'):
+    #             # Replace 'http' with 'https' only if it starts with 'http'
+    #             data['video'] = video_url.replace('http', 'https', 1)
+
+    #         return data
+    #     except Exception as e:
+    #         # Optionally log the exception e
+    #         return data
 
 
 class HeroSectionWriteSerializers(serializers.ModelSerializer):
